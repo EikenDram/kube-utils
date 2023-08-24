893e234a86ff130b0c02223994741e73d6ab27e6
Aiken Drum <eikendram@gmail.com>
2023-08-24 16:55:13 +0300

    work #1

--- keycloak/cmd/delete.go
+++ keycloak/cmd/delete.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -10,15 +8,25 @@ import (
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete users in Keycloak from a text file",
-	Long:  `Delete users in Keycloak from a text file`,
+	Long: `Delete users in Keycloak from a text file
+		By default will only disable users,
+		to permanently delete users use --permanent flag`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("delete called")
+		delete()
 	},
 }
+
+var permanent bool = false
 
 func init() {
 	rootCmd.AddCommand(deleteCmd)
 
 	deleteCmd.Flags().StringVarP(&textFile, "file", "f", "", "Name of text file to process")
 	deleteCmd.MarkFlagRequired("file")
+	deleteCmd.Flags().BoolVarP(&permanent, "permanent", "p", false, "Permanently delete users instead of disabling them")
+
+}
+
+func delete() {
+	//
 }
